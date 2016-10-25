c60d7892805c355e8e978653578358f31baeaf79
Pierre Bellec <pierre.bellec@gmail.com>
2016-10-25 11:29:03 -0400

    Forcing precision to float in the minc reader.

--- commands/read_write/niak_read_minc.m
+++ commands/read_write/niak_read_minc.m
@@ -1,45 +1,33 @@
-function [hdr,vol] = niak_read_minc(file_name,precision_data)
+function [hdr,vol] = niak_read_minc(file_name)
 % Read 3D or 3D+t data in MINC format.
 % http://www.bic.mni.mcgill.ca/software/minc/
 %
-% SYNTAX:
-% [HDR,VOL] = NIAK_READ_MINC(FILE_NAME)
-% 
-% _________________________________________________________________________
-% INPUT:
+% SYNTAX: [HDR,VOL] = NIAK_READ_MINC(FILE_NAME)
+%   FILE_NAME (string) a 3D+t or 3D minc file.
+%   HDR (structure) a description of the data. See NIAK_READ_VOL 
+%     and NIAK_READ_HDR_MINC for details.
+%   VOL (3D+t or 3D array of double) the fMRI or MRI data.
 %
-% FILE_NAME         
-%       (string) a 3D+t or 3D minc file.
-%
-% _________________________________________________________________________
-% OUTPUT:
-%
-% VOL           
-%       (3D+t or 3D array of double) the fMRI or MRI data.
-%
-% HDR
-%       a structure containing a description of the data. See NIAK_READ_VOL 
-%       and NIAK_READ_HDR_MINC for details.
-%
-% _________________________________________________________________________
 % SEE ALSO:
+%   NIAK_READ_HDR_MINC, NIAK_WRITE_MINC, NIAK_READ_VOL, NIAK_WRITE_VOL
 %
-% NIAK_READ_HDR_MINC, NIAK_WRITE_MINC, NIAK_READ_VOL, NIAK_WRITE_VOL
-%
-% _________________________________________________________________________
 % COMMENTS: 
-%
-% Use shell commands MINCINFO (for minc1), MINCHEADER and MINCTORAW which 
-% requires a proper install of minc tools. This function is
-% creating temporary files. If it does not work, try to change the location 
-% of temporary files using the GB_NIAK_TMP variable defined in 
-% the NIAK_GB_VARS function.
+%   Use shell commands MINCINFO (for minc1), MINCHEADER and MINCTORAW which 
+%   requires a proper install of minc tools. This function is
+%   creating temporary files. If it does not work, try to change the location 
+%   of temporary files using the GB_NIAK_TMP variable defined in 
+%   the NIAK_GB_VARS function.
 % 
-% Copyright (c) Pierre Bellec, Montreal Neurological Institute, 2008.
-% Maintainer : pbellec@bic.mni.mcgill.ca
 % See licensing information in the code.
+% Maintainer : pierre.bellec@criugm.qc.ca
 % Keywords : medical imaging, I/O, reader, minc
 
+% Copyright (c) Pierre Bellec, Sebastien Lavoie-Courchesne
+% Montreal Neurological Institute, 2008-2010
+% Centre de recherche de l'institut de geriatrie de Montreal, 
+% Department of Computer Science and Operations Research
+% University of Montreal, Quebec, Canada, 2010-2016
+%
 % Permission is hereby granted, free of charge, to any person obtaining a copy
 % of this software and associated documentation files (the "Software"), to deal
 % in the Software without restriction, including without limitation the rights
@@ -58,10 +46,8 @@ function [hdr,vol] = niak_read_minc(file_name,precision_data)
 % OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 % THE SOFTWARE.
 
-if nargin < 2
-    precision_data = 'float';
-end
-
+precision_data = 'float'; % force data to single point precision
+       
 %% Parsing the header
 hdr = niak_read_hdr_minc(file_name);
 hdr.info.precision = precision_data;
